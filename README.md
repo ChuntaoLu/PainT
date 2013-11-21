0b1d21ae1f10041e69ff59779d626c072d3f1736
ChuntaoLu <chuntaolu13@gmail.com>
2013-11-20 23:27:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+PainT is a simple paint application written in Python using Tkinter.
---
+++ paint.py
@@ -0,0 +1,84 @@
+from Tkinter import *
+from tkColorChooser import askcolor
+__author__ = 'luffy'
+
+
+class Paint(object):
+
+    DEFAULT_PEN_SIZE = 5.0
+    DEFAULT_COLOR = 'black'
+
+    def __init__(self):
+        self.root = Tk()
+
+        self.pen_button = Button(self.root, text='pen', command=self.use_pen)
+        self.pen_button.grid(row=0, column=0)
+
+        self.brush_button = Button(self.root, text='brush', command=self.use_brush)
+        self.brush_button.grid(row=0, column=1)
+
+        self.color_button = Button(self.root, text='color', command=self.choose_color)
+        self.color_button.grid(row=0, column=2)
+
+        self.eraser_button = Button(self.root, text='eraser', command=self.use_eraser)
+        self.eraser_button.grid(row=0, column=3)
+
+        self.c = Canvas(self.root, bg='white', width=600, height=600)
+        self.c.grid(row=1, columnspan=4)
+
+        self.setup()
+        self.root.mainloop()
+
+    def setup(self):
+        self.old_x = None
+        self.old_y = None
+        self.line_width = self.DEFAULT_PEN_SIZE
+        self.color = self.DEFAULT_COLOR
+        self.eraser_on = False
+        self.c.bind('<B1-Motion>', self.paint)
+        self.c.bind('<ButtonRelease-1>', self.reset)
+
+    def use_pen(self):
+        self.activate_button(self.pen_button)
+        self.line_width = self.DEFAULT_PEN_SIZE
+
+    def use_brush(self):
+        self.activate_button(self.brush_button)
+        #TODO: change pen size
+        self.line_width = 10.0
+
+    def choose_color(self):
+        self.eraser_on = False
+        self.color = askcolor()[1]
+
+    def use_eraser(self):
+        self.activate_button(self.eraser_button, eraser_mode=True)
+        #TODO: change eraser size
+        self.line_width = 10
+
+    #TODO: reset canvas
+    #TODO: undo and redo
+
+    def activate_button(self, a_button, eraser_mode=False):
+        a_button.config(relief=SUNKEN)
+        deactivated_buttons = {self.pen_button, self.brush_button,
+                               self.eraser_button} - {a_button}
+        for button in deactivated_buttons:
+            button.config(relief=RAISED)
+        self.eraser_on = eraser_mode
+
+    def paint(self, event):
+        paint_color = 'white' if self.eraser_on else self.color
+        if self.old_x and self.old_y:
+            self.c.create_line(self.old_x, self.old_y, event.x, event.y,
+                               width=self.line_width, fill=paint_color,
+                               capstyle=ROUND, smooth=TRUE, splinesteps=36)
+        self.old_x = event.x
+        self.old_y = event.y
+
+    def reset(self, event):
+        self.old_x, self.old_y = None, None
+
+
+if __name__ == '__main__':
+    ge = Paint()
